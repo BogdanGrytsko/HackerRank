9986bd08bc36395c80e701cbb4caa6cc850b1264
BogdanGrytsko <bgrytsko@gmail.com>
2020-01-01 14:52:10 +0200

    Special String Again.

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,75 +1,60 @@
-﻿using System.Collections.Generic;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
 using System;
 
 class Solution
 {
 
-    // Complete the freqQuery function below.
-    static IEnumerable<int> freqQuery(List<List<int>> queries)
+    // Complete the substrCount function below.
+    static long substrCount(int n, string s)
     {
-        var valDic = new Dictionary<int, long>();
-        var freqDic = new Dictionary<long, HashSet<int>>();
-        for (int j = 0; j < queries.Count; j++)
+        var cnt = 0;
+        for (int i = 0; i < s.Length; i++)
         {
-            var query = queries[j];
-            var val = query[1];
-            if (query[0] == 1)
-            {
-                long prevFreq = -1;
-                if (!valDic.ContainsKey(val) || valDic[val] == 0)
-                    valDic[val] = 1;
-                else
-                {
-                    prevFreq = valDic[val];
-                    valDic[val]++;
-                }
-                var freq = valDic[val];
-                if (!freqDic.ContainsKey(freq))
-                    freqDic.Add(freq, new HashSet<int> { val });
-                else
-                    freqDic[freq].Add(val);
-
-                if (prevFreq != -1)
-                    freqDic[prevFreq].Remove(val);
-            }
-            else if (query[0] == 2)
-            {
-                if (!valDic.ContainsKey(val) || valDic[val] == 0) continue;
-                var freq = valDic[val];
-                valDic[val]--;
-                freqDic[freq].Remove(val);
-                if (freq > 1)
-                    freqDic[freq - 1].Add(val);
-            }
-            else if (query[0] == 3)
-            {
-                HashSet<int> freqSet;
-                if (freqDic.TryGetValue(val, out freqSet) && freqSet.Any())
-                    yield return 1;
-                else
-                    yield return 0;
-            }
+            var startC = s[i];
+            var subCntL = SameCntLength(s, startC, i);
+            cnt += subCntL * (subCntL + 1) / 2;
+            var subCntR = SameCntLength(s, startC, i + subCntL + 1);
+            cnt += Math.Min(subCntL, subCntR);
+            i += subCntL - 1;
         }
+        return cnt;
+    }
+
+    private static int SameCntLength(string s, char startC, int start)
+    {
+        var sameCnt = 0;
+        for (int j = start; j < s.Length; j++)
+        {
+            if (s[j] == startC)
+                sameCnt++;
+            else
+                break;
+        }
+        return sameCnt;
     }
 
     static void Main(string[] args)
     {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int q = Convert.ToInt32(Console.ReadLine().Trim());
+        int n = Convert.ToInt32(Console.ReadLine());
 
-        List<List<int>> queries = new List<List<int>>();
+        string s = Console.ReadLine();
 
-        for (int i = 0; i < q; i++)
-        {
-            queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
-        }
+        long result = substrCount(n, s);
 
-        List<int> ans = freqQuery(queries).ToList();
-
-        textWriter.WriteLine(String.Join("\n", ans));
+        textWriter.WriteLine(result);
 
         textWriter.Flush();
         textWriter.Close();
