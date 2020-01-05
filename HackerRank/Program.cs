7102f96460e15a1869ff673681962fb8458cf045
BogdanGrytsko <bgrytsko@gmail.com>
2020-01-05 22:38:46 +0200

    Minimum Time Required solved.

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -11,104 +11,59 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
-using HackerRank;
 
 class Solution
 {
-    static long triplets(int[] a, int[] b, int[] c)
+
+    // Complete the minTime function below.
+    static long minTime(long[] machines, long goal)
     {
-        return new TripleSum().Solve(a, b, c);
+        Array.Sort(machines);
+        return minTime(machines, goal, 0, machines[0] * goal);
     }
 
-    public class TripleSum
+    static long minTime(long[] machines, long goal, long l, long r)
     {
-        public long Solve(int[] a, int[] b, int[] c)
+        var mid = (l + r) / 2;
+        long res = -1;
+        while (l < r)
         {
-            a = new SortedSet<int>(a).ToArray();
-            b = new SortedSet<int>(b).ToArray();
-            c = new SortedSet<int>(c).ToArray();
-            Array.Sort(a);
-            Array.Sort(b);
-            Array.Sort(c);
-            long cnt = 0;
-            for (int i = 0; i < b.Length; i++)
-            {
-                var el = b[i];
-                var smallerCnta = FindSmallerEqualCnt(a, el);
-                var smallerCntc = FindSmallerEqualCnt(c, el);
-                cnt += (long)smallerCnta * smallerCntc;
-            }
-            return cnt;
-        }
-
-        public static int FindSmallerEqualCnt(int[] arr, int elem)
-        {
-            return FindSmallerEqualCnt(arr, elem, 0, arr.Length - 1);
-        }
-
-        private static int FindSmallerEqualCnt(int[] arr, int elem, int l, int r)
-        {
-            var mid = (l + r) / 2;
-            if (arr[mid] <= elem)
-            {
-                if (mid == arr.Length - 1 || arr[mid + 1] > elem)
-                    return mid + 1;
-                return FindSmallerEqualCnt(arr, elem, mid + 1, r);
-            }
+            mid = (l + r) / 2;
+            var g = CalcResult(machines, mid);
+            if (g < goal)
+                l = mid + 1;
             else
             {
-                if (mid == 0)
-                    return 0;
-                return FindSmallerEqualCnt(arr, elem, l, mid);
+                res = mid;
+                r = mid;
             }
         }
+        return res;
+    }
 
-        public static int FindBiggerEqualCnt(int[] arr, int elem)
+    private static long CalcResult(long[] machines, long time)
+    {
+        long produced = 0;
+        foreach (var machine in machines)
         {
-            return FindBiggerEqualCnt(arr, elem, 0, arr.Length - 1);
+            produced += time / machine;
         }
-
-        private static int FindBiggerEqualCnt(int[] arr, int elem, int l, int r)
-        {
-            var mid = (l + r) / 2;
-            if (arr[mid] < elem)
-            {
-                if (mid == arr.Length - 1)
-                    return 0;
-                return FindBiggerEqualCnt(arr, elem, mid + 1, r);
-            }
-            else
-            {
-                if (mid == 0 || arr[mid - 1] < elem)
-                    return arr.Length - mid;
-                return FindBiggerEqualCnt(arr, elem, l, mid);
-            }
-        }
+        return produced;
     }
 
     static void Main(string[] args)
     {
-        var path = Environment.GetEnvironmentVariable("OUTPUT_PATH");
-        TextWriter textWriter = new StreamWriter(path, true);
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        var lines = File.ReadAllLines(path.Replace("Results", "Input"));
-        string[] lenaLenbLenc = lines[0].Split(' ');
+        string[] nGoal = Console.ReadLine().Split(' ');
 
-        int lena = Convert.ToInt32(lenaLenbLenc[0]);
+        int n = Convert.ToInt32(nGoal[0]);
 
-        int lenb = Convert.ToInt32(lenaLenbLenc[1]);
+        long goal = Convert.ToInt64(nGoal[1]);
 
-        int lenc = Convert.ToInt32(lenaLenbLenc[2]);
-
-        int[] arra = Array.ConvertAll(lines[1].Split(' '), arraTemp => Convert.ToInt32(arraTemp))
+        long[] machines = Array.ConvertAll(Console.ReadLine().Split(' '), machinesTemp => Convert.ToInt64(machinesTemp))
         ;
-
-        int[] arrb = Array.ConvertAll(lines[2].Split(' '), arrbTemp => Convert.ToInt32(arrbTemp))
-        ;
-
-        int[] arrc = Array.ConvertAll(lines[3].Split(' '), arrcTemp => Convert.ToInt32(arrcTemp))
-        ;
-        long ans = triplets(arra, arrb, arrc);
+        long ans = minTime(machines, goal);
 
         textWriter.WriteLine(ans);
 
