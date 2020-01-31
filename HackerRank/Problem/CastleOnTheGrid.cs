8e50ce676f2641394c2e38c3c2d6310ff46df406
BogdanGrytsko <bgrytsko@gmail.com>
2020-01-31 17:46:07 +0200

    Castle on the grid solution.

---
+++ HackerRank/Problem/CastleOnTheGrid.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Problem
+{
+    public class CastleOnTheGrid
+    {
+        public static int minimumMoves(string[] grid, int startX, int startY, int goalX, int goalY)
+        {
+            var matrix = new int[grid.Length, grid.Length];
+            for (int i = 0; i < grid.Length; i++)
+            {
+                var row = grid[i];
+                for (int j = 0; j < row.Length; j++)
+                {
+                    if (row[j] == 'X')
+                        matrix[i, j] = -1;
+                }
+            }
+            var queue = new Queue<(int, int, int)>();
+            queue.Enqueue((startX, startY, 1));
+            BFS(matrix, queue, (goalX, goalY));
+
+            return matrix[goalX, goalY] - 1;
+        }
+
+        private static void BFS(int[,] matrix, Queue<(int, int, int)> queue, (int, int) goal)
+        {
+            while (queue.Any())
+            {
+                var (x, y, mark) = queue.Dequeue();
+                if (matrix[x, y] != 0) continue;
+                matrix[x, y] = mark;
+                if (x == goal.Item1 && y == goal.Item2)
+                    return;
+                foreach (var p in GetNeibours(matrix, (x, y)))
+                {
+                    Enqueue(matrix, queue, (p.x, p.y, mark + 1));
+                }
+                BFS(matrix, queue, goal);
+            }
+        }
+
+        private static IEnumerable<(int x, int y)> GetNeibours(int[,] matrix, (int x, int y) p)
+        {
+            var n = matrix.GetLength(0);
+            var m = matrix.GetLength(1);
+            for (int i = p.x + 1; i < n; i++)
+            {
+                var c = (i, p.y);
+                if (InBound(matrix, c))
+                    yield return c;
+                else
+                    break;
+            }
+
+            for (int i = p.x - 1; i >= 0; i--)
+            {
+                var c = (i, p.y);
+                if (InBound(matrix, c))
+                    yield return c;
+                else
+                    break;
+            }
+
+            for (int i = p.y + 1; i < m; i++)
+            {
+                var c = (p.x, i);
+                if (InBound(matrix, c))
+                    yield return c;
+                else
+                    break;
+            }
+
+            for (int i = p.y - 1; i >= 0; i--)
+            {
+                var c = (p.x, i);
+                if (InBound(matrix, c))
+                    yield return c;
+                else
+                    break;
+            }
+        }
+
+        private static bool InBound(int[,] matrix, (int x, int y) p)
+        {
+            return p.x >= 0 && p.x < matrix.GetLength(0) && p.y >= 0 && p.y < matrix.GetLength(1) && matrix[p.x, p.y] == 0;
+        }
+
+        private static void Enqueue(int[,] matrix, Queue<(int, int, int)> queue, (int x, int y, int mark) p)
+        {
+            if (!InBound(matrix, (p.x, p.y)))
+                return;
+            queue.Enqueue(p);
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -11,87 +11,13 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
-using HackerRank;
+using HackerRank.Problem;
 
 class Solution
 {
 
     // Complete the minimumMoves function below.
-    static int minimumMoves(string[] grid, int startX, int startY, int goalX, int goalY)
-    {
-        var matrix = new int[grid.Length, grid.Length];
-        for (int i = 0; i < grid.Length; i++)
-        {
-            var row = grid[i];
-            for (int j = 0; j < row.Length; j++)
-            {
-                if (row[j] == 'X')
-                    matrix[i, j] = -1;
-            }
-        }
-        var queue = new Queue<(int, int, int)>();
-        queue.Enqueue((startX, startY, 1));
-        BFS(matrix, queue, (goalX, goalY));
 
-        var dirChange = 0;
-        var curr = (goalX, goalY);
-        var currMark = matrix[goalX, goalY];
-        var currDir = 0;
-        while (!curr.Equals((startX, startY)))
-        {
-            foreach (var p in GetNeibours(curr, matrix.GetLength(0), matrix.GetLength(1)))
-            {
-                if (matrix[p.x, p.y] == currMark - 1)
-                {
-                    currMark--;
-                    curr = (p.x, p.y);
-                    var pDir = Math.Abs(p.dir) <= 1 ? 1 : 2;
-                    if (pDir != currDir)
-                        dirChange++;
-                    currDir = pDir;
-                    continue;
-                }
-            }
-        }
-        Util.WriteMatrix(matrix);
-        return dirChange;
-    }
-
-    private static void BFS(int[,] matrix, Queue<(int, int, int)> queue, (int, int) goal)
-    {
-        while (queue.Any())
-        {
-            var (x, y, mark) = queue.Dequeue();
-            if (matrix[x, y] != 0) continue;
-            matrix[x, y] = mark;
-            if (x == goal.Item1 && y == goal.Item2)
-                return;
-            foreach (var p in GetNeibours((x, y), matrix.GetLength(0), matrix.GetLength(1)))
-            {
-                Enqueue(matrix, queue, (p.x, p.y, mark + 1));
-            }
-            BFS(matrix, queue, goal);
-        }
-    }
-
-    private static IEnumerable<(int x, int y, int dir)> GetNeibours((int x, int y) p, int n, int m)
-    {
-        if (p.x + 1 < n)
-            yield return (p.x + 1, p.y, 1);
-        if (p.x - 1 >= 0)
-            yield return (p.x - 1, p.y, -1);
-        if (p.y + 1 < m)
-            yield return (p.x, p.y + 1, 2);
-        if (p.y - 1 >= 0)
-            yield return (p.x , p.y - 1, -2);
-    }
-
-    private static void Enqueue(int[,] matrix, Queue<(int, int, int)> queue, (int x, int y, int mark) p)
-    {
-        if (p.x < 0 || p.x >= matrix.GetLength(0) || p.y < 0 || p.y >= matrix.GetLength(1) || matrix[p.x, p.y] != 0)
-            return;
-        queue.Enqueue(p);
-    }
 
     static void Main(string[] args)
     {
@@ -117,7 +43,7 @@ class Solution
 
         int goalY = Convert.ToInt32(startXStartY[3]);
 
-        int result = minimumMoves(grid, startX, startY, goalX, goalY);
+        int result = CastleOnTheGrid.minimumMoves(grid, startX, startY, goalX, goalY);
 
         textWriter.WriteLine(result);
 
