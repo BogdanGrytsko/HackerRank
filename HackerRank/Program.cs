07eaf9d209f7f4a18f174c1fec6d3b4acd9f2b6f
BogdanGrytsko <bgrytsko@gmail.com>
2020-02-01 18:42:47 +0200

    Roads and libraries

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -11,41 +11,150 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
-using HackerRank.Problem;
 
 class Solution
 {
 
-    // Complete the minimumMoves function below.
+    // Complete the roadsAndLibraries function below.
+    static long roadsAndLibraries(int n, int c_lib, int c_road, int[][] cities)
+    {
+        if (c_lib <= c_road)
+            return (long)c_lib * n;
+        long cost = 0;
+        var graph = new Tree(cities, new int[n]);
+        for (int i = 1; i < graph.Nodes.Count; i++)
+        {
+            var node = graph.Nodes[i];
+            if (node.Visited) continue;
+            var cnt = graph.DFSCount(i);
+            cost += (cnt - 1) * c_road + c_lib;
+        }
+        return cost;
+    }
 
+    public class Tree
+    {
+        public class Node
+        {
+            public int ParentIdx { get; set; }
+            public long Sum { get; set; }
+            public int Value { get; set; }
+            public bool Visited { get; set; }
+            public int Depth { get; set; }
+            public int HeavyIdx { get; set; }
+            public int HeadIdx { get; set; }
+            public int PosIdx { get; set; }
+            public List<int> Edges { get; set; }
+
+            public int Count { get; set; }
+
+            public Node(int val)
+            {
+                Value = val;
+                Sum = val;
+                Edges = new List<int>();
+                HeavyIdx = -1;
+                Count = 1;
+            }
+
+            public override string ToString()
+            {
+                return $"V: {Value}, S: {Sum}, C: {Count}, E: {Edges.Count}";
+            }
+        }
+
+        public Tree(int[][] edges, int[] c)
+        {
+            //to start numeration from 1
+            Nodes.Add(new Node(-1));
+            for (int i = 0; i < c.Length; i++)
+                Nodes.Add(new Node(c[i]));
+            foreach (var edge in edges)
+            {
+                Nodes[edge[0]].Edges.Add(edge[1]);
+                Nodes[edge[1]].Edges.Add(edge[0]);
+            }
+            RootIdx = 1;
+        }
+
+        public Tree(IEnumerable<Node> nodes, int rootIdx)
+        {
+            RootIdx = rootIdx;
+            Nodes = nodes.ToList();
+        }
+
+        public int RootIdx { get; private set; }
+
+        public void ResetVisited()
+        {
+            foreach (var node in Nodes)
+            {
+                node.Visited = false;
+            }
+        }
+
+        public List<Node> Nodes { get; } = new List<Node>();
+
+        public Node this[int nodeIdx]
+        {
+            get
+            {
+                return Nodes[nodeIdx];
+            }
+        }
+
+        public long DFSSum(int nodeIdx)
+        {
+            var node = Nodes[nodeIdx];
+            if (node.Visited)
+                return 0;
+            node.Visited = true;
+            foreach (var edge in node.Edges)
+                node.Sum += DFSSum(edge);
+            return node.Sum;
+        }
+
+        public int DFSCount(int nodeIdx)
+        {
+            var node = Nodes[nodeIdx];
+            if (node.Visited)
+                return 0;
+            node.Visited = true;
+            foreach (var edge in node.Edges)
+                node.Count += DFSCount(edge);
+            return node.Count;
+        }
+    }
 
     static void Main(string[] args)
     {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int n = Convert.ToInt32(Console.ReadLine());
+        int q = Convert.ToInt32(Console.ReadLine());
 
-        string[] grid = new string[n];
-
-        for (int i = 0; i < n; i++)
+        for (int qItr = 0; qItr < q; qItr++)
         {
-            string gridItem = Console.ReadLine();
-            grid[i] = gridItem;
+            string[] nmC_libC_road = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nmC_libC_road[0]);
+
+            int m = Convert.ToInt32(nmC_libC_road[1]);
+
+            int c_lib = Convert.ToInt32(nmC_libC_road[2]);
+
+            int c_road = Convert.ToInt32(nmC_libC_road[3]);
+
+            int[][] cities = new int[m][];
+
+            for (int i = 0; i < m; i++)
+            {
+                cities[i] = Array.ConvertAll(Console.ReadLine().Split(' '), citiesTemp => Convert.ToInt32(citiesTemp));
+            }
+
+            long result = roadsAndLibraries(n, c_lib, c_road, cities);
+
+            textWriter.WriteLine(result);
         }
-
-        string[] startXStartY = Console.ReadLine().Split(' ');
-
-        int startX = Convert.ToInt32(startXStartY[0]);
-
-        int startY = Convert.ToInt32(startXStartY[1]);
-
-        int goalX = Convert.ToInt32(startXStartY[2]);
-
-        int goalY = Convert.ToInt32(startXStartY[3]);
-
-        int result = CastleOnTheGrid.minimumMoves(grid, startX, startY, goalX, goalY);
-
-        textWriter.WriteLine(result);
 
         textWriter.Flush();
         textWriter.Close();
