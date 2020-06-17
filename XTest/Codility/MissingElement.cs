9ce475c7d2fb5cdb5e1023eb43ba09c14f399077
BogdanGrytsko <bgrytsko@gmail.com>
2020-06-17 23:37:32 +0300

    Missing element

---
+++ XTest/Codility/MissingElement.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Xunit;
+// ReSharper disable InconsistentNaming
+
+namespace XTest.Codility
+{
+    public class MissingElement
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(4, solution(new []{ 2, 3, 1, 5}));
+        }
+
+        [Fact]
+        public void Test_Last()
+        {
+            Assert.Equal(5, solution(new[] { 2, 3, 1, 4 }));
+        }
+
+        [Fact]
+        public void Empty_Array()
+        {
+            Assert.Equal(1, solution(new int[0]));
+        }
+
+        public int solution(int[] A)
+        {
+            var arr = Enumerable.Range(1, A.Length + 1);
+            return arr.Except(A).Single();
+        }
+    }
+}
