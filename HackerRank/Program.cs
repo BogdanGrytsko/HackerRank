c47efb1f3a3ad960b4ebc3980582b29bdea5baee
BogdanGrytsko <bgrytsko@gmail.com>
2020-06-17 23:29:24 +0300

    Codility -> FrogJump.

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,107 +1,10 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
 
 class Solution
 {
-
-    // Complete the formingMagicSquare function below.
-    static int formingMagicSquare(int[][] s)
-    {
-        var list = new List<List<List<int>>> {
-            new List<List<int>>
-            {
-                new List<int> { 8, 1 ,6 },
-                new List<int> { 3, 5 ,7 },
-                new List<int> { 4, 9 ,2 },
-            },
-            new List<List<int>>
-            {
-                new List<int> { 6, 1 ,8 },
-                new List<int> { 7, 5 ,3 },
-                new List<int> { 2, 9 ,4 },
-            },
-            new List<List<int>>
-            {
-                new List<int> { 4, 9 ,2 },
-                new List<int> { 3, 5 ,7 },
-                new List<int> { 8, 1 ,6 },
-            },
-            new List<List<int>>
-            {
-                new List<int> { 2, 9 ,4 },
-                new List<int> { 7, 5 ,3 },
-                new List<int> { 6, 1 ,8 },
-            },
-            new List<List<int>>
-            {
-                new List<int> { 8, 3 ,4 },
-                new List<int> { 1, 5 ,9 },
-                new List<int> { 6, 7 ,2 },
-            },
-            new List<List<int>>
-            {
-                new List<int> { 4, 3 ,8 },
-                new List<int> { 9, 5 ,1 },
-                new List<int> { 2, 7 ,6 },
-            },
-            new List<List<int>>
-            {
-                new List<int> { 6, 7 ,2 },
-                new List<int> { 1, 5 ,9 },
-                new List<int> { 8, 3 ,4 },
-            },
-            new List<List<int>>
-            {
-                new List<int> { 2, 7 ,6 },
-                new List<int> { 9, 5 ,1 },
-                new List<int> { 4, 3 ,8 },
-            },
-        };
-
-        var minDist = int.MaxValue;
-        var dist = 0;
-        foreach (var item in list)
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    dist += Math.Abs(item[i][j] - s[i][j]);
-                }
-            }
-            minDist = Math.Min(minDist, dist);
-            dist = 0;
-        }
-        return minDist;
-    }
-
     static void Main(string[] args)
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int[][] s = new int[3][];
-
-        for (int i = 0; i < 3; i++)
-        {
-            s[i] = Array.ConvertAll(Console.ReadLine().Split(' '), sTemp => Convert.ToInt32(sTemp));
-        }
-
-        int result = formingMagicSquare(s);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
     }
 }
---
+++ XTest/Codility/FrogJump.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+// ReSharper disable InconsistentNaming
+
+namespace XTest.Codility
+{
+    public class FrogJump
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(3, solution(10, 85, 30));
+        }
+
+        [Fact]
+        public void Same_Position()
+        {
+            Assert.Equal(0, solution(10, 10, 30));
+        }
+
+        [Fact]
+        public void Exact_Jump()
+        {
+            Assert.Equal(2, solution(10, 80, 35));
+        }
+
+        [Fact]
+        public void BIG_D()
+        {
+            Assert.Equal(1, solution(10, 80, 1000000000));
+        }
+
+        private static int solution(int X, int Y, int D)
+        {
+            var dist = Y - X;
+            var steps = dist / D;
+            if (dist % D != 0)
+                steps++;
+            return steps;
+        }
+    }
+}
