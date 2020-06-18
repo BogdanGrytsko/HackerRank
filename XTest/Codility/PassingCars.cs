0b7936770648895ab6ada0daf9cd6c9d8d8081de
BogdanGrytsko <bgrytsko@gmail.com>
2020-06-18 19:17:31 +0300

    PassingCars

---
+++ XTest/Codility/PassingCars.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace XTest.Codility
+{
+    public class PassingCars
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(5, Solution(new []{0, 1, 0, 1, 1}));
+        }
+
+        public int Solution(int[] A)
+        {
+            var prefix = new int[A.Length];
+            int sum = 0;
+            for (int i = prefix.Length - 1; i >= 0; i--)
+            {
+                if (i != prefix.Length - 1)
+                    prefix[i] = prefix[i + 1];
+                if (A[i] == 1)
+                {
+                    prefix[i]++;
+                }
+                else
+                {
+                    sum += prefix[i];
+                    if (sum > 1000000000)
+                        return -1;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
