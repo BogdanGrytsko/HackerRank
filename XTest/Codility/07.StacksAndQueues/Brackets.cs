967a9dbc4f1835a60e3148c62f2c68ff5e494950
BogdanGrytsko <bgrytsko@gmail.com>
2020-06-18 23:25:00 +0300

    Brackets

---
+++ XTest/Codility/07.StacksAndQueues/Brackets.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace XTest.Codility._07.StacksAndQueues
+{
+    public class Brackets
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(1, Solution("{[()()]}"));
+        }
+
+        [Fact]
+        public void Test_2()
+        {
+            Assert.Equal(0, Solution("([)()]"));
+        }
+
+        public int Solution(String S)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in S)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                    stack.Push(c);
+                else
+                {
+                    if (!stack.Any())
+                        return 0;
+                    var x = stack.Pop();
+                    if (x == '(' && c != ')')
+                        return 0;
+                    if (x == '[' && c != ']')
+                        return 0;
+                    if (x == '{' && c != '}')
+                        return 0;
+                }
+            }
+
+            return stack.Any() ? 0 : 1;
+        }
+    }
+}
