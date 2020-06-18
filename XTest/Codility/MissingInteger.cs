639a77f78b8da59cf2d0ece65350283c3bf6a9f1
BogdanGrytsko <bgrytsko@gmail.com>
2020-06-18 18:24:20 +0300

    MissingInteger

---
+++ XTest/Codility/MissingInteger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace XTest.Codility
+{
+    public class MissingInteger
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(5, Solution(new []{ 1, 3, 6, 4, 1, 2 }));
+            Assert.Equal(4, Solution(new[] {1, 2, 3}));
+            Assert.Equal(1, Solution(new[] { -3, -1 }));
+        }
+
+        public int Solution(int[] A)
+        {
+            var x = A.Where(a => a > 0).Distinct().OrderBy(a => a).ToArray();
+            if (!x.Any())
+                return 1;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] != i + 1)
+                    return i + 1;
+            }
+
+            return x.Length + 1;
+        }
+    }
+}
