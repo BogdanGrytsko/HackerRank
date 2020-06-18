fe236413a1a9133632e29a4b2c334bccafa17090
BogdanGrytsko <bgrytsko@gmail.com>
2020-06-18 23:11:13 +0300

    Nesting

---
+++ XTest/Codility/07.StacksAndQueues/Nesting.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace XTest.Codility._07.StacksAndQueues
+{
+    public class Nesting
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(1, Solution("(()(())())"));
+        }
+
+        [Fact]
+        public void Test_2()
+        {
+            Assert.Equal(0, Solution("())"));
+        }
+
+        [Fact]
+        public void Test_3()
+        {
+            Assert.Equal(0, Solution("()(()()(((()())(()()))"));
+        }
+
+        public int Solution(String S)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in S)
+            {
+                if (c == '(')
+                    stack.Push(c);
+                else if (c == ')')
+                {
+                    if (!stack.Any())
+                        return 0;
+                    stack.Pop();
+                }
+            }
+
+            return stack.Any() ? 0 : 1;
+        }
+    }
+}
