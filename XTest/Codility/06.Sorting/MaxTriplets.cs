6c99424860491d0e14ff11616df8746a409e3116
BogdanGrytsko <bgrytsko@gmail.com>
2020-06-18 20:20:27 +0300

    Fixed for negatives.

--- XTest/Codility/06.Sorting/MaxTriplets.cs
+++ XTest/Codility/06.Sorting/MaxTriplets.cs
@@ -9,14 +9,22 @@ namespace XTest.Codility._06.Sorting
         public void Test()
         {
             Assert.Equal(60, Solution(new []{-3, 1, 2, -2, 5, 6}));
-            Assert.Equal(0, Solution(new[] { -3, -10, 0, -2, 5, 6 }));
+            Assert.Equal(180, Solution(new[] { -3, -10, 0, -2, 5, 6 }));
+        }
+
+        [Fact]
+        public void Negative_Test()
+        {
+            Assert.Equal(20, Solution(new []{-5, -2, 1, 1, 2}));
         }
 
         public int Solution(int[] A)
         {
             Array.Sort(A);
             var n = A.Length - 1;
-            return A[n] * A[n - 1] * A[n - 2];
+            var f = A[n] * A[n - 1] * A[n - 2];
+            var s = A[0] * A[1] * A[n];
+            return Math.Max(f, s);
         }
     }
 }
