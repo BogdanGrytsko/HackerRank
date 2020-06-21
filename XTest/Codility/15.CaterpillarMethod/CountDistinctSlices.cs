4b7886f30b3b4a33e83a8c565211885b35abcce6
BogdanGrytsko <bgrytsko@gmail.com>
2020-06-21 23:19:04 +0300

    CountDistinctSlices

---
+++ XTest/Codility/15.CaterpillarMethod/CountDistinctSlices.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace XTest.Codility._15.CaterpillarMethod
+{
+    public class CountDistinctSlices
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(9, Solution(6, new[] {3, 4, 5, 5, 2}));
+        }
+
+        public int Solution(int M, int[] A)
+        {
+            var cnt = 0;
+            int start = 0, i = 0;
+            var set = new HashSet<int>();
+            while (start < A.Length)
+            {
+                while (i < A.Length && !set.Contains(A[i]))
+                {
+                    set.Add(A[i]);
+                    i++;
+                }
+
+                cnt += set.Count;
+                if (cnt > 1000000000)
+                    return 1000000000;
+                set.Remove(A[start]);
+                start++;
+            }
+
+            return cnt;
+        }
+    }
+}
