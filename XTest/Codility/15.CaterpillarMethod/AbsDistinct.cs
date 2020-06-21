b812670cd7ef7701809bde55d31ab6c65f2ed8b3
BogdanGrytsko <bgrytsko@gmail.com>
2020-06-21 22:52:53 +0300

    AbsDistinct

---
+++ XTest/Codility/15.CaterpillarMethod/AbsDistinct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace XTest.Codility._15.CaterpillarMethod
+{
+    public class AbsDistinct
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(5, Solution(new[] { -5, -3, -1, 0, 3, 6}));
+        }
+
+        [Fact]
+        public void Int_Min_Test()
+        {
+            Assert.Equal(6, Solution(new[] { -5, -3, -1, 0, 3, 6, int.MinValue }));
+        }
+
+        public int Solution(int[] A)
+        {
+            var cnt = A.Where(a => a != int.MinValue).Select(Math.Abs).Distinct().Count();
+            if (A.Contains(int.MinValue))
+                cnt++;
+            return cnt;
+        }
+    }
+}
