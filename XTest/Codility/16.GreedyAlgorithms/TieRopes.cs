999a8d25d6e61534de97a65578db2891ee24171b
BogdanGrytsko <bgrytsko@gmail.com>
2020-07-03 13:51:56 +0300

    TieRopes

---
+++ XTest/Codility/16.GreedyAlgorithms/TieRopes.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace XTest.Codility._16.GreedyAlgorithms
+{
+    public class TieRopes
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(3, Solution(4, new []{1, 2, 3, 4, 1, 1, 3}));
+        }
+
+        [Fact]
+        public void Cant_Produce_Test()
+        {
+            Assert.Equal(0, Solution(100, new[] {1, 2, 3}));
+        }
+
+        public int Solution(int K, int[] A)
+        {
+            var cnt = 0;
+            var len = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                len += A[i];
+                if (len >= K)
+                {
+                    cnt++;
+                    len = 0;
+                }
+            }
+            return cnt;
+        }
+    }
+}
